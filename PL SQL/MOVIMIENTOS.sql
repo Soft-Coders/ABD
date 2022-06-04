90472c1aca34853d09d332b4c0d4bcb1ee59725d
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-06-04 18:36:50 +0200

    Separado script tabla MOVIMIENTOS

---
+++ PL SQL/MOVIMIENTOS.sql
@@ -0,0 +1,16 @@
+CREATE TABLE movimientos (
+    numero              VARCHAR2(20) NOT NULL,
+    numero_tarjeta      NUMBER(16,0) NOT NULL,
+    fecha               DATE NOT NULL,
+    cantidad            NUMBER(9,0),
+    estado              VARCHAR2(20) NOT NULL,
+    modo                VARCHAR2(20),
+    tipo_emisor         VARCHAR2(20)
+)
+LOGGING;
+
+ALTER TABLE movimientos ADD CONSTRAINT movimientos_pk PRIMARY KEY ( numero );
+
+ALTER TABLE movimientos ADD CONSTRAINT tarjeta_fk FOREIGN KEY ( numero_tarjeta )
+        REFERENCES tarjetas ( numero )
+    NOT DEFERRABLE;
