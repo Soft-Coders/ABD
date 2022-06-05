17fc55c9d26dc9f8ef17f63554e13c3e384806f5
pablohud <pablohud04@icloud.com>
2022-06-05 21:14:39 +0200

    añadido J_CAMBIO_EURO

---
+++ PL SQL/J_CAMBIO_EURO.sql
@@ -0,0 +1,26 @@
+
+CREATE OR REPLACE PROCEDURE P_CAMBIOEURO
+IS
+BEGIN
+    UPDATE DIVISA
+        SET 
+            DIVISA.CAMBIO_EURO = V_COTIZACIONES.CAMBIOEURO
+        WHERE
+            DIVISA.ABREVIATURA = V_COTIZACIONES.ABREVIATURA
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+END P_CAMBIOEURO;
+
+DBMS_SCHEDULER.CREATE_JOB(
+    job_name            => 'J_CAMBIO_EURO',
+    job_type            => 'PLSQL_BLOCK',
+    job_action          => 'P_CAMBIOEURO;',
+    start_date          => TIMESTAMP '2022-06-06 00:05:00',
+    repeat_interval     => 'FREQ=DAILY; INTERVAL=1',
+    end_date            => null,
+    enabled             => TRUE,
+    comments            => 'Actualiza el atributo cambio euro de la tabla divisa tomando el valor de la vista v_cotizaciones cada día a las 00:05'
+)
+
