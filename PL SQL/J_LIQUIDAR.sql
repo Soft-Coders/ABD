2965fbbf7d117f5638307abeeb14eb176424aa3d
Marta <49495616+martame18@users.noreply.github.com>
2022-06-06 18:07:41 +0200

    Añadidos los atributos que faltaban

--- PL SQL/J_LIQUIDAR.sql
+++ PL SQL/J_LIQUIDAR.sql
@@ -2,8 +2,11 @@
 BEGIN
   DBMS_SCHEDULER.CREATE_JOB (
    job_name           =>  'J_LIQUIDAR',
+   job_type            => 'PLSQL_BLOCK',
    job_action         =>  'P_COBRO',
    start_date         =>  '01-JUN-22 00.00.00 AM',
+   end_date            => null,
+   enabled             => TRUE,
    repeat_interval    =>  'FREQ=MONTHLY');
 END;
 /
